964d53fcc85c12a0c9a306dd1dab4a01feb17bcc
clogerie <66565287+clogerie@users.noreply.github.com>
2022-06-29 18:05:19 -0400

    prefixed routes in api/index.js

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const blogRoutes = require("./blogRoutes");
+const commentsRoutes = require("./commentsRoutes");
+const userRoutes = require("./userRoutes");
+
+router.use('/blogs', blogRoutes);
+router.use('/comments', commentsRoutes);
+router.use('/users', usersRoutes);
+
+module.exports = router;
