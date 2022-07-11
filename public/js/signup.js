992a31e28d1bf1f8da651cae396d0e7a6f6ae4d9
clogerie <66565287+clogerie@users.noreply.github.com>
2022-07-11 12:50:02 -0400

    added logic to allow users to signup

---
+++ public/js/signup.js
@@ -0,0 +1,24 @@
+const signUpFormHandler = async (event) => {
+    console.log('signUpFormHandler');
+      event.preventDefault();
+      const email = document.querySelector('#email-signup').value.trim();  
+      const username = document.querySelector('#username-signup').value.trim();
+      const password = document.querySelector('#password-signup').value.trim();
+    
+      if (email && username && password) {
+        console.log("signup");
+        const response = await fetch('/api/users/signup', {
+          method: 'POST',
+          body: JSON.stringify({ username, email, password }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+    
+        if (response.ok) {
+          document.location.replace('/');
+        } else {
+          alert(response.statusText);
+        }
+      }
+    };
+  
+    document.getElementById("submitBtn").addEventListener("click", signUpFormHandler);
