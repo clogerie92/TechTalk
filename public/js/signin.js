4895fb732b6a8879e46e076d361bfc9afdb747c3
clogerie <66565287+clogerie@users.noreply.github.com>
2022-07-11 12:40:04 -0400

    added logic to signin.js to allow users to signin

--- public/js/signin.js
+++ public/js/signin.js
@@ -1,5 +1,21 @@
 const signinFormHandler = async (event) => {
     event.preventDefault();
+    const username = document.querySelector("#username-singin").value.trim();
+    const password = document.querySelector("#password-singin").value.trim();
 
-    const 
-}
+    if(username && password) {
+        const response = await fetch("/api/users/signin", {
+            method: "POST",
+            body: JSON.stringify({username, password}),
+            headers: {"Content-Type": "application/json"}
+        });
+
+        if(response.ok) {
+            document.location.replace("/dashboard");
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector("#submitBtn").addEventListener("click", signinFormHandler);
